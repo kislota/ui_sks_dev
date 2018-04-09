afd7eb57923cc2db8d59b1cdfc466ba833c7cc08
kislota <kislotakill@gmail.com>
2018-04-09 11:31:48 +0300

    add componets

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,27 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { UserComponent } from './user/user.component';
+import { UsersComponent } from './users/users.component';
+import { ClientComponent } from './client/client.component';
+import { ClientsComponent } from './clients/clients.component';
+import { OrderComponent } from './order/order.component';
+import { OrdersComponent } from './orders/orders.component';
+import { CashboxComponent } from './cashbox/cashbox.component';
+import { StockComponent } from './stock/stock.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserComponent,
+    UsersComponent,
+    ClientComponent,
+    ClientsComponent,
+    OrderComponent,
+    OrdersComponent,
+    CashboxComponent,
+    StockComponent
   ],
   imports: [
     BrowserModule
